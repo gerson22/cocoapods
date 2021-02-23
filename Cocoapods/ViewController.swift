a9cd66d1e246df41c76dfe5ad2002837ae80949b
gerson97 <g.isaias22@outlook.com>
2021-02-23 13:50:16 -0600

    [INIT] Initial Commit

--- Cocoapods/ViewController.swift
+++ Cocoapods/ViewController.swift
@@ -6,12 +6,25 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.request()
+    }
+    
+    func request() {
+        AF.request("https://jsonplaceholder.typicode.com/posts/1",
+            method: .put).responseJSON(){ response in
+            debugPrint(response)
+        }
+    }
+    
+    func buildStars() {
+    
     }
 
 
